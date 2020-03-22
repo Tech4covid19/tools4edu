a16b7fa1fe77713d2547f9f75f18627ee3b69cc6
Fábio Ferreira <ffcfpten@gmail.com>
2020-03-22 20:46:53 +0000

    refactor: navbar active items

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  url = '';
 
-  constructor() { }
+  constructor(private router: Router) {
+    router.events.subscribe((route) => {
+      if(route instanceof NavigationEnd) {
+        if (route.url && route.url.length > 0) {
+          this.url = route.url.slice(1);
+          console.log(this.url)
+        }
+      }
+    });
+  }
 
   ngOnInit() {
   }
