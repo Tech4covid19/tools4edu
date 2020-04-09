ae266bd6324b86ecc412a14ce9c32d6311a43477
Joao Rocha <jsfrocha@gmail.com>
2020-04-09 03:53:40 +0100

    - CSS Deep in Content-Article; - "Voltar para Conteudos"; - Order dos Stakeholder Cards na Homepage

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -26,6 +26,7 @@ export class HomeComponent implements OnInit {
         stakeholders{
           id,
           title,
+          order
         }
        }`,
      }) .valueChanges.pipe(map((result:any) =>
